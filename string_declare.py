b814bf88c4405fca87fec97bf8ebeecfe9187152
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-14 03:01:50 +0600

    Add files via upload

---
+++ string_declare.py
@@ -0,0 +1,12 @@
+user_name= "shirin"
+password= '123456'
+print(user_name,password)
+
+print (user_name.upper())
+
+
+paragraph = '''lorem In publishing and graphic design,
+ Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+ Lorem ipsum may be used as a placeholder before final copy is available.'''
+
+print(paragraph.lower())
