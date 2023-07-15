74a3ba6bb892bd56e8e6b7999aee05f3f775e62d
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-15 16:18:58 +0600

    Add files via upload

---
+++ looping_tuple.py
@@ -0,0 +1,12 @@
+fruits=("mango","apple","orange" ,1,2,1.5,["a","b"])
+for fruit in fruits:
+    print(fruit)
+print(len(fruits))
+
+fruits=list(fruits)
+print(fruits)
+i="mango"
+n=fruits
+while i<=n:
+    print(i)
+    i=i+ 1
