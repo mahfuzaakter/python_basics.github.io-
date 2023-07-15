44626ae1b14976d72c759014967aba9d5981c297
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-16 01:54:03 +0600

    Add files via upload

---
+++ dictionary_declary.py
@@ -0,0 +1,9 @@
+student={"name":"mahfuza akter","age": 10,"courses":["python","java","c++"]}
+
+
+#empty dictionary
+empty_dictionary= {}
+print(type(empty_dictionary))
+
+empty_dictionary= dict()
+print(type(empty_dictionary))
