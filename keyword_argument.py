076df45e51f5f5288f9b00dc46bd8ad31fac5f26
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-17 03:52:12 +0600

    Add files via upload

---
+++ keyword_argument.py
@@ -0,0 +1,5 @@
+def uder_info(first_name, last_name):
+    print("user first name is {}".format(first_name))
+    print("user last name is {}".format(last_name))
+
+uder_info(first_name="mahfuza",last_name="shirin")
