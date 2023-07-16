49c0ce459cca030ea0828bb0afe631c908b422b8
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-17 04:16:43 +0600

    Add files via upload

---
+++ python_lambda.py
@@ -0,0 +1,9 @@
+# ##def square(a):
+#     return a**2
+
+square = lambda a:a**2    # ekta argument pass korar jonno
+print(square(2))
+
+# duita argument store krar jonno
+average = lambda a,b : (a+b)/2
+print(average(4,6))
