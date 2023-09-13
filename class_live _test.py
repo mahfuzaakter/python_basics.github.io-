acb86252cacda7fdd02e192238eedbefbdd82223
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-09-13 22:50:15 +0600

    Add files via upload

---
+++ class_live _test.py
@@ -0,0 +1,27 @@
+# class Student:
+#     def __init__(self, name, roll_no):
+#         self.name = name
+#         self.roll_no = roll_no
+    
+#     def display(self):
+#         print("Name:", self.name)
+#         print("Roll No:", self.roll_no)
+
+# # Create an object of the Student class
+# student_object = Student("John", 2)
+
+# # Call the display method to show the values
+# student_object.display()
+
+class Student:
+         def __init__(self, name, roll_no):
+                self.name= name
+                self.roll_no= roll_no
+         def  Display(self):
+               print("Name:", self.name)
+               print("Roll:", self.roll_no)
+object= Student("John", 2)
+object.Display()
+    
+
+
